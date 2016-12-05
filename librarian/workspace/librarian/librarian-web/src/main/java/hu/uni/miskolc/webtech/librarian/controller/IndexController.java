8d4ed0c94bcedfeeefbcaa38f6e27fb877f535f2
Alex Toth <tothalex95@gmail.com>
2016-12-05 19:40:36 +0100

    my own files

--- librarian/workspace/librarian/librarian-web/src/main/java/hu/uni/miskolc/webtech/librarian/controller/IndexController.java
+++ librarian/workspace/librarian/librarian-web/src/main/java/hu/uni/miskolc/webtech/librarian/controller/IndexController.java
@@ -1,18 +1,52 @@
 package hu.uni.miskolc.webtech.librarian.controller;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import hu.uni.miskolc.webtech.librarian.controller.dto.BookAssembler;
+import hu.uni.miskolc.webtech.librarian.controller.dto.BookDTO;
+import hu.uni.miskolc.webtech.librarian.model.Author;
+import hu.uni.miskolc.webtech.librarian.model.Book;
+import hu.uni.miskolc.webtech.librarian.persist.AuthorNotFoundException;
+import hu.uni.miskolc.webtech.librarian.service.BookManagerService;
 
 
 
 @Controller
 public class IndexController {
 	
-	@RequestMapping(value={"", "/", "/home", "/index"})
-	public ModelAndView homePage(){
+	private static final Logger LOG = LogManager.getLogger();
+	
+	@Autowired
+	private BookManagerService manager;
+	
+	@RequestMapping(value={"", "/", "/index"})
+	public ModelAndView indexPage(){
 		ModelAndView result = new ModelAndView("index");
+		return result;
+	}
+	
+	@RequestMapping("/home")
+	public ModelAndView homePage() {
+		ModelAndView result = new ModelAndView("home");
+		return result;
+	}
+	
+	@RequestMapping("/about")
+	public ModelAndView aboutPage() {
+		ModelAndView result = new ModelAndView("about");
 		return result;
 	}
 
@@ -20,5 +54,55 @@ public class IndexController {
 	public @ResponseBody String sayHello(){
 		return "Hello World";
 	}
+	
+	@RequestMapping("/listauthors")
+	public ModelAndView listOfAuthors() {
+		return new ModelAndView("authorList");
+	}
 
+	@RequestMapping(value={"/newauthor", "/updateauthor"})
+	public ModelAndView modifyAuthor() {
+		ModelAndView result = new ModelAndView("authorForm");
+		return result;
+	}
+	
+	@RequestMapping("/listbooks")
+	public ModelAndView listOfBooks() {
+		return new ModelAndView("bookList");
+	}
+	
+	@RequestMapping(value={"/newbook", "/updatebook"})
+	public ModelAndView modifyBook() {
+		ModelAndView result = new ModelAndView("bookForm");
+		return result;
+	}
+	
+	@RequestMapping(value={"/book/insert"}, method=RequestMethod.POST, consumes="application/json")
+	@ResponseBody
+	public void insertBook(@RequestBody BookDTO book) {
+		try {
+			manager.addBook(BookAssembler.assembleAuthor(book));
+		} catch (Exception e) {
+			LOG.info(e.getMessage());
+		}
+	}
+	
+	@RequestMapping(value={"/book/update"}, method=RequestMethod.POST, consumes="application/json")
+	@ResponseBody
+	public void updateBook(@RequestBody BookDTO book) {
+		try {
+			manager.updateBook(BookAssembler.assembleAuthor(book));
+		} catch (Exception e) {
+			LOG.info(e.getMessage());
+		}
+	}
+	
+	@RequestMapping("/books/{id}")
+	public @ResponseBody Collection<Book> listBooksOfAuthor(@PathVariable Integer id) throws AuthorNotFoundException {
+		List<Author> authors = manager.queryAuthors().stream().filter(a -> a.getAuthorID() == id).collect(Collectors.toList());
+		if (authors.size() == 1)
+			return manager.queryBooks(authors.get(0));
+		throw new AuthorNotFoundException();
+	}
+	
 }
---
+++ librarian/workspace/librarian/librarian-web/src/main/webapp/js/utils.js
@@ -0,0 +1,83 @@
+function convertDate(dateToCheck) {
+	var date = new Date(dateToCheck);
+	var turningDate = new Date(1582, 9, 15);
+	if (date.getTime() < turningDate.getTime()) {
+		date.setDate(date.getDate() - 10);
+	}
+	var century = 1500
+	while (date.getFullYear() < century) {
+		if ((century % 400) != 0) {
+			date.setDate(date.getDate() + 1);
+		}
+		century = century - 100;
+	}
+	var day = date.getDate();
+	var dayStr = day < 10 ? '0' + day : day;
+	var month = date.getMonth() + 1;
+	var monthStr = month < 10 ? '0' + month : month;
+	var year = date.getYear() + 1900;
+	var yearStr = year;
+	var formatedTime = yearStr + '.' + monthStr + '.' + dayStr + '.';
+	return formatedTime;
+}
+
+$.fn.serializeObject = function() {
+	var o = {};
+	var a = this.serializeArray();
+	$.each(a, function() {
+		if (o[this.name] !== undefined) {
+			if (!o[this.name].push) {
+				o[this.name] = [ o[this.name] ];
+			}
+			o[this.name].push(this.value || '');
+		} else {
+			o[this.name] = this.value || '';
+		}
+	});
+	return o;
+};
+
+function getParameter(name) {
+	if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)'))
+			.exec(location.search))
+		return decodeURIComponent(name[1]);
+};
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function getValidDateString(date) {
+	var day = date.getDate();
+	var dayStr = day < 10 ? '0' + day : day;
+	var month = date.getMonth() + 1;
+	var monthStr = month < 10 ? '0' + month : month;
+	var year = date.getYear() + 1900;
+	var yearStr = year;
+	return yearStr + '-' + monthStr + '-' + dayStr;
+}
+
+function buttonClicked(target) {
+	var splittedTarget = target.split(':');
+	console.log(splittedTarget[0] + ' ---> ' + splittedTarget[1]);
+	setCookie('id', splittedTarget[0], 1);
+	$('#content').load(splittedTarget[1]);
+};
